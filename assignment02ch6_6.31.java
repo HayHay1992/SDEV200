ab69b173ffd65a613c6b14bea084711950e8f24b
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-01-21 15:20:16 -0500

    Update and rename assignment 02 ch6 - 6.31 to assignment02ch6_6.31.java

---
+++ assignment02ch6_6.31.java
@@ -0,0 +1,9 @@
+6.3.1 How do you define a method? How do you invoke a method?
+
+//define a method
+public static void main(String[] args) {
+//Your code goes here.
+}
+
+//invoke a method
+ClassName.methodName
