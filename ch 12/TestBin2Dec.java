00843623a49805c83af6e2dd3d4aa2967324fff1
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-01-28 14:54:14 -0500

    Create TestBin2Dec.java

---
+++ ch 12/TestBin2Dec.java
@@ -0,0 +1,15 @@
+public class TestBin2Dec {
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter a binary string
+		System.out.print("Enter binary: ");
+		try {
+			System.out.println(Exercise_12_07.bin2Dec(input.nextLine()));
+		}
+		catch (NumberFormatException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+}
