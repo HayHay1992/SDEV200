f46c4585e5506f7eae692f4c65e53699458f5675
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-02-25 12:59:15 -0500

    Update NoughtsAndCrosses.java

--- Final Project/NoughtsAndCrosses.java
+++ Final Project/NoughtsAndCrosses.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class NoughtsAndCrosses implements ActionListener{
 
-    // Create game frame of NoughtsAndCrosses AKA XO
+    // Create game frame of NoughtsAndCrosses AKA TicTacToe
     // Include two players
 
     Random random = new Random();
@@ -19,7 +19,28 @@ public class NoughtsAndCrosses implements ActionListener{
 
     NoughtsAndCrosses(){
 
+        // Set frame size
+        // Set frame colour
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900,900);
+        frame.getContentPane().setBackground(new Colour(10,10,10));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Colour(26,25,25));
+        textfield.setForeground(new Colour(245,242,242));
+        textfield.setFont(new Font("Ink Free",Font.Bold,85));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("NoughtsAndCrosses")
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,900,100);
+
+        title_panel.add(textfield);
+        frame.add(title_panel),BorderLayout.NORTH);
+        
 
     }
 
