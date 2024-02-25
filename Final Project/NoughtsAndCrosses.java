792f3f981f3c85936e2bfd1553a67d4d35cba8ad
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-02-25 13:09:47 -0500

    Update NoughtsAndCrosses.java

--- Final Project/NoughtsAndCrosses.java
+++ Final Project/NoughtsAndCrosses.java
@@ -32,14 +32,14 @@ public class NoughtsAndCrosses implements ActionListener{
         textfield.setForeground(new Colour(245,242,242));
         textfield.setFont(new Font("Ink Free",Font.Bold,85));
         textfield.setHorizontalAlignment(JLabel.CENTER);
-        textfield.setText("NoughtsAndCrosses")
+        textfield.setText("NoughtsAndCrosses");
         textfield.setOpaque(true);
 
         title_panel.setLayout(new BorderLayout());
         title_panel.setBounds(0,0,900,100);
 
         title_panel.add(textfield);
-        frame.add(title_panel),BorderLayout.NORTH);
+        frame.add(title_panel,BorderLayout.NORTH);
         
 
     }
